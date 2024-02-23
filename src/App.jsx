cc458757771bb2d539e4a2bdad0dd7b141934634
fikireab-m <fikireab.meku@gmail.com>
2024-02-23 14:21:38 +0300

    changing to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.scss';
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <Counter />
+    </main>
   );
 }
 
