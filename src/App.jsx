6df4db8881e475dd838e87976ad41039a8a45118
fikireab-m <fikireab.meku@gmail.com>
2024-02-25 19:35:23 +0300

    using async thunk to fetch posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import PostsList from "./PostsList";
+import PostsList from "./pages/PostsList";
 import AddPostForm from "./components/AddPostForm";
 
 function App() {
--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,20 +1,44 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "../features/posts/postsSlice";
-import PostTile from "../components/PostTile";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "../features/posts/postsSlice";
+import { useEffect } from "react";
+import PostsExcerpt from "../components/PostsExcerpt";
 
 const PostsList = () => {
+  const dispatch = useDispatch();
+
   const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
 
-  const renderedPosts = orderedPosts.map((post) => <PostTile post={post} key={post.id}/>);
+  let content;
+  if (postStatus === "loading") {
+    content = <p> Loading...</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
