f336541ed122bd12a96b76d51bf871b3c497a7e6
fikireab-m <fikireab.meku@gmail.com>
2024-02-25 20:29:17 +0300

    routing and post detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import PostsList from "./pages/PostsList";
 import AddPostForm from "./pages/AddPostForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import SinglePostPage from "./pages/SinglePostPage";
 
 function App() {
   return (
-    <main className="App">
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PostsList />} />
+
+          <Route path="post">
+            <Route index element={<AddPostForm />} />
+            <Route path=":postId" element={<SinglePostPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
